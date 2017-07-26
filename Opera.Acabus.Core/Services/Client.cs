dbfce0db7ada95ea54eeb349da33f9bf39b9f9f9
Javier de Jesús Flores Mondragón <javi_azul_93@hotmail.com>
2017-07-25 22:19:53 -0500

    Se agregan clases de comunicación TCP/IP

---
+++ Opera.Acabus.Core/Services/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opera.Acabus.Core.Services
+{
+    /// <summary>
+    /// Esta clase estática gestiona las conexiones al servidor de aplicación de todo el sistema.
+    /// Provee de toda la funcionalidad que su identificador de aplicación le permite.
+    /// </summary>
+    public static class Client
+    {
+    }
+}
