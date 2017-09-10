c30433ed94df57e362fe3b6b5e26c6aa69270233
Javier Flores <javi_azul_93@hotmail.com>
2017-09-10 13:14:09 +0000

    Revert "Interpretación de una consulta con filtros y constantes
    Se elimina la clase ChainHelper"

--- InnSyTech.Standard/Database/Linq/ExpressionVisitor.cs
+++ InnSyTech.Standard/Database/Linq/ExpressionVisitor.cs
@@ -11,9 +11,8 @@ namespace InnSyTech.Standard.Database.Linq
 {
     internal class ExpressionVisitor : System.Linq.Expressions.ExpressionVisitor
     {
-
+        private List<ChainHelper> _chains = new List<ChainHelper>();
         private DbStatementDefinition _statementDefinition = new DbStatementDefinition();
-        private List<Tuple<String, DbFieldDefinition>> _fieldLabels = new List<Tuple<string, DbFieldDefinition>>();
         private IDbDialect _dialect;
         private StringBuilder _statement;
 
@@ -24,55 +23,12 @@ namespace InnSyTech.Standard.Database.Linq
 
             Visit(expression);
 
-            ProcessDefinition(_statementDefinition);
+            throw new NotImplementedException();
+
+            TranslateChains();
+            TranslateFields();
 
             return _statement.ToString();
-        }
-
-        private void ProcessDefinition(DbStatementDefinition statementDefinition)
-        {
-            int count = 0;
-            foreach (var statement in _statementDefinition)
-                foreach (var entity in statement.Entities)
-                    count = ProcessEntity(count, entity);
-
-            foreach (var label in _fieldLabels)
-                _statement.Replace(label.Item1, label.Item2.Entity.Alias);
-
-            _statement
-                .Replace(", {{fields}}", "")
-                .Replace(" {{entities}}", "");
-        }
-
-        private int ProcessEntity(int count, DbEntityDefinition entity)
-        {
-            StringBuilder fieldsString = new StringBuilder();
-            String entityName = DbHelper.GetEntityName(entity.EntityType);
-
-            entity.Alias = String.Format("T{0}", count++);
-
-            foreach (var dbFields in DbHelper.GetFields(entity.EntityType))
-                fieldsString.AppendFormat("{1}.{0} {1}_{0}, ", dbFields.Name, entity.Alias);
-
-            _statement.Replace("{{fields}}", String.Format("{0}{1}", fieldsString.ToString(), "{{fields}}"));
-
-            if (entity.Alias.Equals("T0"))
-                _statement.Replace("{{entities}}", String.Format("{0} T0 {1}", entityName, "{{entities}}"));
-            else
-                _statement.Replace("{{entities}}", string.Format(
-                    "LEFT JOIN {0} {1} ON {1}.{2} = {3}.{4} {5}",
-                    entityName,
-                    entity.Alias,
-                    DbHelper.GetPrimaryKey(entity.EntityType).Name,
-                    entity.DependencyEntity.Alias,
-                    entity.DependencyMember.GetFieldName(),
-                    "{{entities}}"
-                ));
-
-            foreach (var subEntity in entity.DependentsEntities)
-                count = ProcessEntity(count, subEntity);
-
-            return count;
         }
 
         protected override Expression VisitBinary(BinaryExpression b)
@@ -138,9 +94,9 @@ namespace InnSyTech.Standard.Database.Linq
         {
             if (c.Value is IQueryable q)
             {
-                _statement.Append("SELECT {{fields}} FROM {{entities}} ");
-
-                _statementDefinition.AddEntity(new DbEntityDefinition() { EntityType = q.ElementType });
+                _statement.Append("SELECT {fields} FROM ")
+                    .Append("{{entities}}");
+                _statementDefinition.AddEntity(new DbEntityDefinition() { Entity = q.ElementType });
             }
             else if (c.Value == null)
                 _statement.Append("NULL");
@@ -181,18 +137,14 @@ namespace InnSyTech.Standard.Database.Linq
                 var entityReference = new DbEntityDefinition();
                 var entity = entityReference;
 
-                DbFieldDefinition fieldDef = entityReference.CreateMember(m.Member);
-                String label = String.Format("{{FL{0}}}", _fieldLabels.Count);
-
-                _selectedList?.Add(fieldDef);
-                _fieldLabels.Add(Tuple.Create(label, fieldDef));
-                _statement.AppendFormat("{0}.{1}", label, fieldDef.GetFieldName());
+                ///_statement.AppendFormat("{{{1}}}.{0}", DbHelper.GetFieldName(m.Member, m.Expression.Type), _chains.Count);
+                _selectedList?.Add(entityReference.CreateMember(m.Member));
 
                 while (expMember != null)
                 {
                     if (DbHelper.IsEntity(expMember.Expression.Type))
                     {
-                        entity.EntityType = expMember.Expression.Type;
+                        entity.Entity = expMember.Expression.Type;
                         if (entity.DependentsEntities.Count > 0)
                             entity.DependentsEntities.First().DependencyMember = entity.CreateMember(expMember.Member);
                     }
@@ -223,17 +175,17 @@ namespace InnSyTech.Standard.Database.Linq
             switch (m.Method.Name)
             {
                 case "Where":
+                    _selectedList = _statementDefinition.Filters;
                     LambdaExpression lambda = (LambdaExpression)StripQuotes(m.Arguments[1]);
                     Visit(m.Arguments[0]);
-                    _statement.Append(" WHERE ");
-                    _selectedList = _statementDefinition.Filters;
+                    // _statement.Append(" WHERE ");
                     Visit(lambda.Body);
                     break;
 
                 case "OrderBy":
+                    _selectedList = _statementDefinition.Orders;
                     Visit(m.Arguments[0]);
                     _statement.Append(" ORDER BY ");
-                    _selectedList = _statementDefinition.Orders;
                     Visit(m.Arguments[1]);
                     break;
 
@@ -246,6 +198,9 @@ namespace InnSyTech.Standard.Database.Linq
                 default:
                     break;
             }
+
+            _selectedList = null;
+
             return m;
         }
 
@@ -281,6 +236,28 @@ namespace InnSyTech.Standard.Database.Linq
             return e;
         }
 
+        //private void LoadReference(MemberInfo instance, int depth, ChainHelper chain = null)
+        //{
+        //    if (depth < 0)
+        //        return;
+
+        //    if (chain == null)
+        //        chain = new ChainHelper();
+
+        //    chain.AddLast(instance);
+
+        //    var references = DbHelper.GetFields(TypeHelper.GetMemberType(instance)).Where(f => f.IsForeignKey);
+
+        //    if (depth == 0 || references.Count() == 0)
+        //    {
+        //        _chains.Add(chain);
+        //        return;
+        //    }
+
+        //    foreach (var item in references)
+        //        LoadReference(item.PropertyInfo as MemberInfo, depth - 1, new ChainHelper() { chain });
+        //}
+
         private void LoadReference(MemberInfo instance, int depth, DbEntityDefinition entity = null)
         {
             if (depth < 0)
@@ -291,7 +268,7 @@ namespace InnSyTech.Standard.Database.Linq
             else
                 entity = entity.CreateDependentsEntity(instance);
 
-            entity.EntityType = TypeHelper.GetMemberType(instance);
+            entity.Entity = TypeHelper.GetMemberType(instance);
 
             var references = DbHelper.GetFields(TypeHelper.GetMemberType(instance)).Where(f => f.IsForeignKey);
 
@@ -304,6 +281,88 @@ namespace InnSyTech.Standard.Database.Linq
             foreach (var reference in references)
                 LoadReference(reference.PropertyInfo as MemberInfo, depth - 1, entity);
 
+        }
+
+        private void TranslateChains()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if (_chains.Count > 0)
+            {
+                var currentChain = 0;
+
+                while (currentChain < _chains.Count)
+                {
+                    var lastChain = _chains[currentChain];
+
+                    foreach (var chain in _chains.SkipWhile(ch => ch != lastChain))
+                        lastChain.GetEqualsNodes(chain);
+
+                    currentChain++;
+                }
+
+                int typesCount = 0;
+
+                foreach (var chain in _chains)
+                    foreach (var type in chain)
+                    {
+                        if (!String.IsNullOrEmpty(type.Alias)) continue;
+                        if (!DbHelper.IsEntity(TypeHelper.GetMemberType(type.Member))) continue;
+
+                        type.Alias = String.Format("T{0}", typesCount);
+                        typesCount++;
+                    }
+
+                builder.AppendFormat(" {0} ", _chains.Merge().ElementAt(0).Alias);
+
+                foreach (var chain in _chains)
+                {
+                    var lastNode = chain[0];
+                    foreach (var entity in chain.Skip(1))
+                    {
+                        if (!builder.ToString().Contains(String.Format(" {0} ", entity.Alias)))
+                        {
+                            var entityType = TypeHelper.GetMemberType(entity.Member);
+
+                            if (!DbHelper.IsEntity(entityType)) continue;
+
+                            var lastEntityType = TypeHelper.GetMemberType(lastNode.Member);
+                            var foreignKey = DbHelper.GetFieldName(entity.Member);
+                            var primaryKey = DbHelper.GetPrimaryKey(entityType);
+
+                            builder.AppendFormat("LEFT OUTER JOIN {4} {5} ON {0}.{1} = {2}.{3} ",
+                                lastNode.Alias,
+                                foreignKey,
+                                entity.Alias,
+                                primaryKey.Name,
+                                DbHelper.GetEntityName(entityType),
+                                entity.Alias);
+                        }
+                        lastNode = entity;
+                    }
+                }
+            }
+
+            _statement.Replace("{joins}", builder.ToString());
+
+            for (int i = 0; i < _chains.Count; i++)
+                _statement.Replace($"{{{i}}}", _chains[i].Last(e => DbHelper.IsEntity(TypeHelper.GetMemberType(e.Member))).Alias);
+        }
+
+        private void TranslateFields()
+        {
+            var entities = _chains
+                .Merge()
+                .Distinct()
+                .Where(e => DbHelper.IsEntity(TypeHelper.GetMemberType(e.Member)));
+
+            StringBuilder fields = new StringBuilder();
+
+            foreach (var entity in entities)
+                foreach (var field in DbHelper.GetFields(TypeHelper.GetMemberType(entity.Member)))
+                    fields.AppendFormat("{0}.{1} {0}_{1}, ", entity.Alias, field.Name);
+
+            _statement.Replace("{fields}", fields.Remove(fields.Length - 2, 2).ToString());
         }
     }
 }
