938a9b54e169a20b510ca1af30651febb8bce1ac
Javier de Jesús Flores Mondragón <javi_azul_93@hotmail.com>
2017-09-10 08:34:36 -0500

    Revert "Revert "Se aplica cambio de nombre a DbUtils --> DbHelper""
    This reverts commit 765030b02094298023159a627cb3d43ee093c075.

--- InnSyTech.Standard/Database/Linq/EvaluatorExpression.cs
+++ InnSyTech.Standard/Database/Linq/EvaluatorExpression.cs
@@ -5,13 +5,13 @@ using System.Linq.Expressions;
 namespace InnSyTech.Standard.Database.Linq
 {
     internal static class EvaluatorExpression
-    {   
+    {
         /// <summary>
-         /// Performs evaluation & replacement of independent sub-trees
-         /// </summary>
-         /// <param name="expression">The root of the expression tree.</param>
-         /// <param name="fnCanBeEvaluated">A function that decides whether a given expression node can be part of the local function.</param>
-         /// <returns>A new tree with sub-trees evaluated and replaced.</returns>
+        /// Performs evaluation & replacement of independent sub-trees
+        /// </summary>
+        /// <param name="expression">The root of the expression tree.</param>
+        /// <param name="fnCanBeEvaluated">A function that decides whether a given expression node can be part of the local function.</param>
+        /// <returns>A new tree with sub-trees evaluated and replaced.</returns>
         public static Expression PartialEval(Expression expression, Func<Expression, bool> fnCanBeEvaluated)
         {
             return new SubtreeEvaluator(new Nominator(fnCanBeEvaluated).Nominate(expression)).Eval(expression);
