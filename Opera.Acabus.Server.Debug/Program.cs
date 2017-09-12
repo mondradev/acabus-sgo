bffc7fe381205cc96abba2baed3b4ebaa21511f4
Javier de Jesús Flores Mondragón <javi_azul_93@hotmail.com>
2017-09-11 23:19:04 -0500

    Prueba de Core v0.1 RC2

--- Opera.Acabus.Server.Debug/Program.cs
+++ Opera.Acabus.Server.Debug/Program.cs
@@ -1,53 +1,46 @@
-﻿using InnSyTech.Standard.Database;
-using InnSyTech.Standard.Database.Linq;
-using InnSyTech.Standard.Database.Sqlite;
-using Opera.Acabus.Core.Models;
-using Opera.Acabus.TrunkMonitor.Models;
+﻿using InnSyTech.Standard.Net.Messenger.Iso8583;
+using Opera.Acabus.Core.Services;
 using System;
-using System.Data.SQLite;
-using System.Diagnostics;
-using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Opera.Acabus.Server.Debug
 {
-    internal class DbDialect : IDbDialect
-    {
-        public string ConnectionString => @"Data Source=C:\Users\javi_\Documents\projects\ACABUS-Control de operacion\Opera.Acabus.Sgo\bin\Release\Resources\acabus_data.dat ; Password=acabus*data*dat";
-
-        public IDbConverter DateTimeConverter => new SQLiteDateTimeConverter();
-
-        public string LastInsertFunctionName => "";
-
-        public int TransactionPerConnection => 1;
-    }
-
     internal class Program
     {
         private static void Main(string[] args)
         {
-            //Message.SetTemplate(Path.Combine(Environment.CurrentDirectory, "acabus.config"));
+            AppMessage.SetTemplate(Path.Combine(Environment.CurrentDirectory, "acabus.config"));
 
-            //Device d = new Device("KVR022901", DeviceType.KVR);
+            var msj = new AppMessage();
+            msj.AddField(10, "Cliente: Hola");
 
-            //var msj = new Message();
-            //msj.AddField(20, d.GetBytes());
+            var task = Task.Run(() =>
+            {
+                AppServer.Initialize();
+            });
 
-            //var d2 = ModelsExtension.GetDevice(msj.GetBytes(20));
+            var task1 = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                AppClient client = new AppClient();
+                var res = client.SendRequest(msj);
+                Console.WriteLine(res);
+                res.AddField(11, "Cliente: Hola de nuevo");
+                res = client.SendRequest(res);
+                Console.WriteLine(res);
+                res.AddField(12, "Cliente: Ok, Adios!");
+                res = client.SendRequest(res);
+                Console.WriteLine(res);
 
-            var db = DbFactory.CreateSession<SQLiteConnection>(new DbDialect());
+                res.AddField(13, "Cliente: Adios :D");
+                res = client.SendRequest(res);
+                Console.WriteLine(res);
+            });
 
-            var query =
-               //db.Read<Device>()
-               db.Read<Link>()
-                .LoadReference(1)
-                //.Execute()
-               // .Where(l => l.StationA.Name == "CENTRO DE CONTROL" || l.StationA.Name == "MICHOACÁN")
-                //.Where(d => d.Type == DeviceType.KVR)
-                .OrderBy(l => l.ID)
-              ;
+            Task.WaitAll(task, task1);
 
-            foreach (var l in query)
-                Trace.WriteLine(l);
         }
     }
 }
