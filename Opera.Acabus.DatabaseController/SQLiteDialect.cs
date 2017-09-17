473442ddc51fb8f7c68498a36f91afdd6b734b25
Javier de Jesús Flores Mondragón <javi_azul_93@hotmail.com>
2017-09-17 00:26:14 -0500

    Se crea proyecto para dll utilizada para el acceso a BD
    Se crea proyecto para la libería utilizada para el accesos a los datos de SQLite.
    
    Elementos de trabajo relacionados: #65

---
+++ Opera.Acabus.DatabaseController/SQLiteDialect.cs
@@ -0,0 +1,18 @@
+﻿using InnSyTech.Standard.Database;
+using InnSyTech.Standard.Database.Sqlite;
+
+namespace Opera.Acabus.DatabaseController
+{
+    public class SQLiteDialect : DbDialectBase
+    {
+        public SQLiteDialect(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override IDbConverter DateTimeConverter => new SQLiteDateTimeConverter();
+
+        public override string LastInsertFunctionName => "last_insert_rowid";
+
+        public override int TransactionPerConnection => 1;
+    }
+}
